a08cc13361a5f40f1009a3b9cb74e442cc7f6d36
HajerJemaa <haje.jemaa4380@istic.ucar.tn>
2025-07-12 10:50:23 +0100

    Initial commit

---
+++ backend/src/main/java/com/HJ/BankReclamation/BankReclamationApplication.java
@@ -0,0 +1,13 @@
+package com.HJ.BankReclamation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankReclamationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankReclamationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/HJ/BankReclamation/BankReclamationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HJ.BankReclamation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankReclamationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
